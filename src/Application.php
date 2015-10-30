ea6099677e67c08e31835bfd3488418484586de1
Matthieu Napoli <matthieu@mnapoli.fr>
2015-10-31 00:05:14 +0100

    Let users customize how the container is created
    The `ContainerFactory` has been merged into `Application` in protected methods. Protected methods allow users to override them to customize how the container (or containerbuilder) are created.

--- src/Application.php
+++ src/Application.php
@@ -2,8 +2,11 @@
 
 namespace Stratify\Framework;
 
+use DI\ContainerBuilder;
 use Interop\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Puli\Repository\Api\ResourceRepository;
+use Puli\UrlGenerator\Api\UrlGenerator;
 use Stratify\Framework\Config\ConfigCompiler;
 use Stratify\Framework\Config\Node;
 use Zend\Diactoros\Response\EmitterInterface;
@@ -30,11 +33,11 @@ class Application
      */
     public function __construct($http, array $modules = [], $config = [])
     {
-        if (! empty($config)) {
+        if (!empty($config)) {
             $modules[] = $config;
         }
 
-        $this->container = ContainerFactory::create($modules);
+        $this->container = $this->createContainer($modules);
 
         /** @var ConfigCompiler $configCompiler */
         $configCompiler = $this->container->get(ConfigCompiler::class);
@@ -56,5 +59,61 @@ class Application
     public function getContainer()
     {
         return $this->container;
+    }
+
+    /**
+     * Override this method to customize how the container is created.
+     *
+     * @param array $modules Array of definition files/arrays
+     */
+    protected function createContainer(array $modules) : ContainerInterface
+    {
+        $containerBuilder = $this->createContainerBuilder($modules);
+
+        return $containerBuilder->build();
+    }
+
+    /**
+     * Override this method to configure the container builder.
+     *
+     * @param array $modules Array of definition files/arrays
+     */
+    protected function createContainerBuilder(array $modules) : ContainerBuilder
+    {
+        $builder = new ContainerBuilder;
+
+        $factoryClass = PULI_FACTORY_CLASS;
+        $puli = new $factoryClass();
+
+        /** @var ResourceRepository $resourceRepository */
+        $resourceRepository = $puli->createRepository();
+        $resourceDiscovery = $puli->createDiscovery($resourceRepository);
+
+        $builder->addDefinitions([
+            'puli.factory' => $puli,
+            ResourceRepository::class => $resourceRepository,
+            UrlGenerator::class => $puli->createUrlGenerator($resourceDiscovery),
+        ]);
+
+        $this->addModule($builder, $resourceRepository, 'stratify');
+
+        foreach ($modules as $module) {
+            $this->addModule($builder, $resourceRepository, $module);
+        }
+
+        return $builder;
+    }
+
+    private function addModule(ContainerBuilder $builder, ResourceRepository $resources, $module)
+    {
+        if (is_string($module)) {
+            // Module name
+            $file = '/' . $module . '/config/config.php';
+            $builder->addDefinitions($resources->get($file)->getFilesystemPath());
+        } else {
+            // Definition array
+            assert(is_array($module));
+            $builder->addDefinitions($module);
+        }
     }
 }
